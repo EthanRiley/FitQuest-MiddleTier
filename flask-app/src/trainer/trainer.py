c32f4925663d69e179fbd0f6aca61c3e3eef6d05
Ziyu Wang <you@example.comc120124@163.com>
2023-04-14 10:49:55 -0400

    user trainer

--- flask-app/src/trainer/trainer.py
+++ flask-app/src/trainer/trainer.py
@@ -3,3 +3,19 @@ import json
 from src import db
 
 trainer = Blueprint('trainer', __name__)
+
+@trainer.route('/hireTrainer', methods=['POST'])
+def hire_new_trainer():
+     the_data = request.json
+     current_app.logger.info(the_data)
+     trainerID=the_data['trainerID']
+     rate=the_data['rate']
+     specialty=the_data['specialty']
+     TrainerName=the_data['TrainerName']
+     query = 'insert into Trainers ( trainerID, rate, specialty, TrainerName) values ("'
+     query +=  trainerID + '", ' +str(rate) + ', "' + specialty + '", "'  + TrainerName + '")'
+     current_app.logger.info(query)
+     cursor = db.get_db().cursor()
+     cursor.execute(query)
+     db.get_db().commit()
+     return "sucesses added"
