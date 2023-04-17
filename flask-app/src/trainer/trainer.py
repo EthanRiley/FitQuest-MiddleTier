a1214a5120338952a5ccf669061440f412c28d08
Ethan Gilworth <gilworthethan@gmail.com>
2023-04-17 19:54:50 -0400

    removed override

--- flask-app/src/trainer/trainer.py
+++ flask-app/src/trainer/trainer.py
@@ -179,21 +179,3 @@ def get_specefic_equipment():
         cursor.execute(query)
         db.get_db().commit()
         return "sucesses"
-
-
-
-@trainer.route('/hireTrainer', methods=['POST'])
-def hire_new_trainer():
-     the_data = request.json
-     current_app.logger.info(the_data)
-     trainerID=the_data['trainerID']
-     rate=the_data['rate']
-     specialty=the_data['specialty']
-     TrainerName=the_data['TrainerName']
-     query = 'insert into Trainers ( trainerID, rate, specialty, TrainerName) values ("'
-     query +=  trainerID + '", ' +str(rate) + ', "' + specialty + '", "'  + TrainerName + '")'
-     current_app.logger.info(query)
-     cursor = db.get_db().cursor()
-     cursor.execute(query)
-     db.get_db().commit()
-     return "sucesses added"
